5d3537a17c7d175a3e454713725a48f369da85cf
阿岛 <16537944+zys060510@user.noreply.gitee.com>
2025-12-31 11:54:15 +0800

    添加了ItemSO物品类

---
+++ My project (1)/Assets/zys/Scripts/SO/ItemSO.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ItemSO : ScriptableObject
+{
+    public string name;
+    public ItemType itemType;
+    public string description;
+    public List<ItemProperty> propertyList;
+    public Sprite icon;
+    public GameObject prefab;
+}
+ 
+public enum ItemType
+{
+    Weapen,
+    Consumable
+}
+
+[Serializable]
+public class ItemProperty
+{
+    public ItemPropertyType propertyType;
+    public int value;
+}
+
+public enum ItemPropertyType
+{
+    HPValue,
+    AttackValue
+}
