f3f238089770101971827d51729627e1c245d216
yujk807 <1969201798@qq.com>
2026-01-04 22:38:59 +0800

    attack

--- My project (1)/Assets/zzh/PlayerMove.cs
+++ My project (1)/Assets/zzh/PlayerMove.cs
@@ -13,6 +13,12 @@ public class PlayerAction : MonoBehaviour
     public Collider2D playerColl;
     public Animator playerAnim;
 
+    // 添加二段跳相关变量
+    private int jumpCount = 0; // 当前跳跃次数
+    private int maxJumpCount = 2; // 最大跳跃次数
+    private bool canDoubleJump = false; // 是否可以进行二段跳
+    private bool isJumping = false; // 是否正在跳跃
+
     void Start()
     {
         playerColl = GetComponent<Collider2D>();
@@ -25,6 +31,21 @@ public class PlayerAction : MonoBehaviour
         PlayerMove();
         PlayerJump();
         isGround = Physics2D.OverlapCircle(foot.position, 0.1f, Ground);
+
+        // 在地面时重置跳跃次数
+        if (isGround)
+        {
+            jumpCount = 0;
+            canDoubleJump = true; // 重置二段跳能力
+            isJumping = false;
+            playerAnim.SetBool("jump", false);
+        }
+
+        // 设置动画参数
+        playerAnim.SetBool("isGrounded", isGround);
+
+        PlayerAttack();
+
     }
 
     void PlayerMove()
@@ -38,16 +59,38 @@ public class PlayerAction : MonoBehaviour
             transform.localScale = new Vector3(faceNum, transform.localScale.y, transform.localScale.z);
         }
     }
+
     void PlayerJump()
     {
-        if (Input.GetButton("Jump"))
+        // 检测跳跃按键按下
+        if (Input.GetButtonDown("Jump"))
         {
-            playerRB.velocity = new Vector2(playerRB.velocity.x, playerJumpSpeed);
-            playerAnim.SetBool("jump", true);
+            // 一段跳：在地面时可以跳跃
+            if (isGround)
+            {
+                playerRB.velocity = new Vector2(playerRB.velocity.x, playerJumpSpeed);
+                jumpCount = 1;
+                isJumping = true;
+                playerAnim.SetBool("jump", true);
+            }
+            // 二段跳：在空中且还有跳跃次数且可以进行二段跳
+            else if (!isGround && jumpCount < maxJumpCount && canDoubleJump)
+            {
+                // 重置垂直速度，使二段跳高度一致
+                playerRB.velocity = new Vector2(playerRB.velocity.x, 0f);
+                playerRB.velocity = new Vector2(playerRB.velocity.x, playerJumpSpeed);
+                jumpCount++;
+                canDoubleJump = false; // 使用完二段跳
+                playerAnim.SetBool("jump", true);
+            }
         }
-        if (isGround)
+    }
+
+    void PlayerAttack()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            playerAnim.SetBool("jump", false);
+            playerAnim.SetTrigger("attack");
         }
     }
 }
